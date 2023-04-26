9fcd097d2b8b34cc98971f5f37747addf969050f
David Briffa <david.briffa.21@um.edu.mt>
2023-04-26 15:36:46 +0200

    View model and Controller

---
+++ nemesis/Controllers/ReportsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nemesis.Models.Interfaces;
+using nemesis.ViewModels;
+
+namespace nemesis.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly IReportRepository _reportRepository;
+
+        public ReportsController(IReportRepository reportRepository)
+        {
+            _reportRepository = reportRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var reports = _reportRepository.getAllReports();
+            var model = new ReportsListViewModel()
+            {
+                TotalReports = reports.Count(),
+                Reports = reports
+            };
+            return View(model);
+        }
+    }
+}
---
+++ nemesis/ViewModels/ReportsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using nemesis.Models;
+
+namespace nemesis.ViewModels
+{
+    public class ReportsListViewModel
+    {
+
+        public int TotalReports { get; set; }
+        public IEnumerable<Report> Reports { get; set; }
+    }
+}
